aa7386a6d9fcebc35ee0350f16b7a41c497bff25
BRPickens <brpickens@gmail.com>
2017-10-06 11:37:12 -0500

    i cant even

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-var host = 'I';
 var myFriends = ['Lovelight', 'Jeff', 'Daniel', 'Tyffani', 'Erica']
 var weaponName = ['Baseball', 'Letter Opener', 'Spoon', 'Cabbage Patch Kids doll', 'Syrup', 'Decorative Pillow', 'Freddy Krueger doll', 'Care Bear Stare (Dont Ask)', 'Lego on the floor', 'Tyler\'s sense of humor', 'Hadouken', 'Banana Gun', 'Angel Arm', 'Burning Finger', 'Spirit Ball', 'Giga Drill Break', 'Falcon Punch', 'Getsuga Tenshou', 'Senbonzakura', 'Death Note'];
-var locationName = ['Gotham donut shop', 'Innovate Depot Room E126', 'bathroom', 'kitchen', 'living room', 'Care-a-Lot', 'Elm Street', 'Namek', 'Kame House', 'Hueco Mundo']
-var accusedGuests = ['Accused']
+var locationsName = ['Gotham donut shop', 'Innovate Depot Room E126', 'bathroom', 'kitchen', 'living room', 'Care-a-Lot', 'Elm Street', 'Namek', 'Kame House', 'Hueco Mundo']
+var accusedHeading;
 
-$(document).ready.function(){
-    for (let i = 0; i < )
-
-
-
-})
-
-
-
-
-
-// function accusedGuests(guestOne, guestTwo, GuestThree, GuestFour, GuestFive) {
-//     console.log(this.me + ' accuse ' +guestOne +', with the ' + weaponName + ' in the ' + locationName + '!' );
-// }
+$(document).ready(function () {
+    for (i = 1; i <=100; i++) {
+        var element = {
+            friend: myFriends[Math.floor((Math.random() * myFriends.length) + 0)],
+            weapons: weaponName[Math.floor((Math.random() * weaponName) + 0)],
+            locations: locationsName[Math.floor((Math.random() * locationsName) + 0)]
+        }
+        accusedHeading
+        }
+    })
 
 
 
@@ -38,18 +33,3 @@ $(document).ready.function(){
 // * Modulus is your friend
 // * ex: 10 mod 10 is 0, 11 mod 10 is 1, 12 mod 10 is 2, ...
 // * You only need to use one loop for this assignment
-
-
-
-
-// var myFriends = ['Wade', 'Keenan', 'Darcell', 'Andrew', 'Barron']
-
-// for (let i = 0; i < myFriends.length; i++) {
-//     console.log(myFriends[i],':');
-//     for(code=99; code > 1; code--) {
-//         console.log(code + ' lines of code in the file,' + code +' lines of code; ' + myFriends[i], ' strikes one out, clears it all out, ' + (code-1) +'lines of code in the file');
-//     }
-//     if (code===1); {
-//         console.log(code + 'line of code in the file,' + code + ' line of code;' + myFriends[i], ' strikes one out, clears it all out,' + (code-1) +' no more lines of code in the file')
-//     }
-// }
